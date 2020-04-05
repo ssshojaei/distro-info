0469f7153b0b9d0c1c4a1a0d28b3fcb480e00ad8
Saleh Shojaei <ordinarysaleh@gmail.com>
2020-04-06 04:17:11 +0430

    upgraded theme functions

--- README.md
+++ README.md
@@ -15,8 +15,8 @@
 **require / import**
 
     // CommonJS
-    const distro = require('distro-info')
-    console.log(distro.name())
+    const { name } = require('distro-info')
+    console.log(name())
 
     // ES6
     import { name } from 'distro-info'
@@ -26,27 +26,63 @@
 
 **name:**
 
-    console.log(distro.name()) //arch
+    console.log(name()) //arch
 
 **desktop:**
 
-    console.log(distro.desktop()) //gnome
+    console.log(desktop()) //gnome
 
 **version:** (DE version)
 
-    console.log(distro.desktop()) //3.36.0
+    console.log(desktop()) //3.36.0
 
 **theme:**
 
-    console.log(distro.theme())
-    //  {
-    //    gtk: 'sierra-dark',
-    //    icon: 'mcmojave-circle',
-    //    font: 'ubvazir 10',
-    //    cursor: 'deepin',
-    //    shell: 'mojave-dark-solid'
-    //  }
+fonts:
 
-**only one case:**
+	theme.fonts.font_name = 'Vazir 10'
+	console.log(theme.fonts.font_name) // Vazir 10
+	
+	theme.fonts.document_font = 'ubvazir 11'
+	console.log(theme.fonts.document_font) // ubvazir 11
+	
+	theme.fonts.monospace_font = 'VazirCode 11'
+	console.log(theme.fonts.monospace_font) // VazirCode 11
+		
+	theme.fonts.font = 'Tanha 10'
+	console.log(theme.fonts.font) // Tanha 10
+	
+background:
+	
+	theme.background = 'file:///home/saleh/background.jpg'
+	console.log(theme.background) // 'file:///home/saleh/background.jpg'
+		
+gtk:
+	
+	theme.gtk.theme = 'Sierra-dark'
+	console.log(theme.gtk.theme) // Sierra-dark
 
-    console.log(distro.theme().gtk)
+	theme.gtk.colorPalette = 'black:white:gray50,...'
+	console.log(theme.gtk.colorPalette) // black:white:gray50,...
+
+icon:
+	
+	theme.icon = 'McMojave-circle'
+	console.log(theme.icon) // 'McMojave-circle'
+	
+cursor:
+	
+	theme.cursor = 'deepin'
+	console.log(theme.cursor) // 'deepin'
+		
+shell:
+	
+	theme.shell = 'Mojave-dark-solid'
+	console.log(theme.shell) // 'Mojave-dark-solid'
+			
+extensions:
+	
+	console.log(theme.extensions.enabled()) // ['caffeine@patapon.info', 'user-theme', ...]
+	theme.extensions.enable('hibernate-status@dromi')
+	theme.extensions.disable('hibernate-status@dromi')
+	
--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const shell = require('shelljs')
 
 const name = () => {
 	const res = shell.exec('lsb_release -i', { silent: true }).stdout
-	return res
-		.split(':')[1]
-		.toLocaleLowerCase()
-		.trim()
+	return res.split(':')[1].toLocaleLowerCase().trim()
 }
 
 const desktop = () => {
@@ -30,21 +27,122 @@ const version = () => {
 	}
 }
 
-const theme = () => {
-	const clean = value => {
-		const res = shell.exec(value, { silent: true }).stdout
-		return res
-			.replace(/'/g, '')
-			.trim()
-			.toLocaleLowerCase()
-	}
-	return {
-		gtk: clean('gsettings get org.gnome.desktop.interface gtk-theme'),
-		icon: clean('gsettings get org.gnome.desktop.interface icon-theme'),
-		font: clean('gsettings get org.gnome.desktop.interface font-name'),
-		cursor: clean('gsettings get org.gnome.desktop.interface cursor-theme'),
-		shell: clean('gsettings get org.gnome.shell.extensions.user-theme name'),
-	}
+const clean = (value) => {
+	const res = shell.exec(value, { silent: true }).stdout
+	return res.replace(/'/g, '').trim()
 }
 
+const theme = {
+	fonts: {
+		get font_name() {
+			return clean('gsettings get org.gnome.desktop.interface font-name')
+		},
+		set font_name(val) {
+			shell.exec(
+				`gsettings set org.gnome.desktop.interface font-name '${val}'`,
+				{ silent: true }
+			)
+		},
+		get document_font() {
+			return clean(
+				'gsettings get org.gnome.desktop.interface document-font-name'
+			)
+		},
+		set document_font(val) {
+			shell.exec(
+				`gsettings set org.gnome.desktop.interface document-font-name '${val}'`,
+				{ silent: true }
+			)
+		},
+		get monospace_font() {
+			return clean(
+				'gsettings get org.gnome.desktop.interface monospace-font-name'
+			)
+		},
+		set monospace_font(val) {
+			shell.exec(
+				`gsettings set org.gnome.desktop.interface monospace-font-name '${val}'`,
+				{ silent: true }
+			)
+		},
+		get font() {
+			return clean('gsettings get org.gnome.desktop.interface font-name')
+		},
+		set font(val) {
+			shell.exec(
+				`gsettings set org.gnome.desktop.interface font-name '${val}'`,
+				{ silent: true }
+			)
+		},
+	},
+	get background() {
+		return clean('gsettings get org.gnome.desktop.background picture-uri')
+	},
+	set background(val) {
+		shell.exec(
+			`gsettings set org.gnome.desktop.background picture-uri '${val}'`,
+			{ silent: true }
+		)
+	},
+	gtk: {
+		get theme() {
+			return clean('gsettings get org.gnome.desktop.interface gtk-theme')
+		},
+		set theme(val) {
+			shell.exec(
+				`gsettings set org.gnome.desktop.interface gtk-theme '${val}'`,
+				{ silent: true }
+			)
+		},
+		get colorPalette() {
+			return clean(
+				'gsettings get org.gnome.desktop.interface gtk-color-palette'
+			)
+		},
+		set colorPalette(val) {
+			shell.exec(
+				`gsettings set org.gnome.desktop.interface gtk-color-palette '${val}'`,
+				{ silent: true }
+			)
+		},
+	},
+	get icon() {
+		return clean('gsettings get org.gnome.desktop.interface icon-theme')
+	},
+	set icon(val) {
+		shell.exec(
+			`gsettings set org.gnome.desktop.interface icon-theme '${val}'`,
+			{ silent: true }
+		)
+	},
+	get cursor() {
+		return clean('gsettings get org.gnome.desktop.interface cursor-theme')
+	},
+	set cursor(val) {
+		shell.exec(
+			`gsettings set org.gnome.desktop.interface cursor-theme '${val}'`,
+			{ silent: true }
+		)
+	},
+	get shell() {
+		return clean('gsettings get org.gnome.shell.extensions.user-theme name')
+	},
+	set shell(val) {
+		shell.exec(
+			`gsettings set org.gnome.shell.extensions.user-theme name '${val}'`,
+			{ silent: true }
+		)
+	},
+	extensions: {
+		enabled() {
+			return clean('gsettings get org.gnome.shell enabled-extensions')
+		},
+		enable(val) {
+			shell.exec(`gnome-shell-extension-tool -e '${val}'`, { silent: true })
+		},
+		disable(val) {
+			shell.exec(`gnome-shell-extension-tool -d '${val}'`, { silent: true })
+		},
+	},
+}
 module.exports = { name, desktop, version, theme }
